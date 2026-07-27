8d977dca7a96200bbbcdf41bb01c8d93b08ec730
souryasharma <kumarsourya120@gmail.com>
2026-07-27 22:42:53 +0530

    Fix splice method error in array assignment

--- 07.arrays/assignment.js
+++ 07.arrays/assignment.js
@@ -1,5 +1,18 @@
-function tosum(a,b){
-console.log("the sum of a and b = ", a+b);
 
-}
-tosum(10,90);
+// Qs. Create an array to store companies -> “Bloomberg”, “Microsoft”,“Uber”, “Google” , “IBM” ,“Netflix”
+
+// a. Remove the first company from the array
+
+// b. Remove Uber & Add Ola in its place
+
+// c. Add Amazon at the end
+
+let companies = ["bloomberg", "microsoft", "uber", "google", "ibm", "netflix"];
+
+companies.shift(); 
+
+companies.splice(1, 1, "ola"); 
+
+companies.push("amazon");
+
+console.log(companies);
