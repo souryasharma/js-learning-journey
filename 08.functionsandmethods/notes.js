4dabf256aefd858cabbff8d945d6a92a57db4c3f
souryasharma <kumarsourya120@gmail.com>
2026-08-02 23:47:17 +0530

    add types of functions

--- 08.functionsandmethods/notes.js
+++ 08.functionsandmethods/notes.js
@@ -11,18 +11,29 @@
 //example
 function sum(a,b){
     //a and b is parameter 
-    console.log("sum of a and b is = " + a+b);
+    console.log("sum of a and b is = "+ (a+b)
+    );
 
 
 } 
 
 
-sum(90,70); // here the 90 and 70 is argument.
-//Parameter: sum (placeholder inside the function).
-//Argument: 90 and 70 (real value given at call time).
-//
-// Return Statement
+sum(90,70);  // here the 90 and 70 is argument.
+  //Parameter: sum (placeholder inside the function).
+  //Argument: 90 and 70 (real value given at call time).
+   //
+ // Return Statement
 
     // The return statement is used to send a result back from a function.
     // When return executes, the function stops running at that point.
     // The returned value can be stored in a variable or used directly.
+    function mul(a,b){
+         let mul=a*b;
+        return mul;
+    }
+   
+let value =mul(2,3);
+console.log(value);
+//types of functions 
+//arrow funtions 
+//it is compact way of writing a function.
